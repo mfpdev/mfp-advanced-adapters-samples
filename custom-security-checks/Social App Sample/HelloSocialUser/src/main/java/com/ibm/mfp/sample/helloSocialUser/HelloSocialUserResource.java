82175cc02fba2aa642f7be318e681fecc6be2e8f
Ishai Borovoy <ISHAIB@il.ibm.com>
2016-03-14 00:10:31 +0200

    Update HelloSocialUserResource.java

--- custom-security-checks/Social App Sample/HelloSocialUser/src/main/java/com/ibm/mfp/sample/helloSocialUser/HelloSocialUserResource.java
+++ custom-security-checks/Social App Sample/HelloSocialUser/src/main/java/com/ibm/mfp/sample/helloSocialUser/HelloSocialUserResource.java
@@ -1,39 +1,37 @@
-/*
- *    Licensed Materials - Property of IBM
- *    5725-I43 (C) Copyright IBM Corp. 2015. All Rights Reserved.
- *    US Government Users Restricted Rights - Use, duplication or
- *    disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+/**
+ * Copyright 2016 IBM Corp.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.ibm.mfp.sample.helloSocialUser;
 
+import com.ibm.mfp.adapter.api.ConfigurationAPI;
+import com.ibm.mfp.adapter.api.OAuthSecurity;
+import com.ibm.mfp.server.registration.external.model.AuthenticatedUser;
 import com.ibm.mfp.server.security.external.resource.AdapterSecurityContext;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Logger;
-
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.core.SecurityContext;
-
-import com.ibm.mfp.adapter.api.ConfigurationAPI;
-import com.ibm.mfp.adapter.api.OAuthSecurity;
+import java.util.Map;
+import java.util.logging.Logger;
 
 @Api(value = "Sample Adapter Resource")
 @Path("/hello")
@@ -55,16 +53,18 @@ public class HelloSocialUserResource {
 
 	/*
 	 * Path for method:
-	 * "<server address>/mfp/api/adapters/HelloSocialUser/resource"
+	 * "<server address>/mfp/api/adapters/HelloSocialUser/hello"
 	 */
 
-	@ApiOperation(value = "Returns 'Hello from resource'", notes = "A basic example of a resource returning a constant string.")
-	@ApiResponses(value = { @ApiResponse(code = 200, message = "Hello message returned") })
+	@ApiOperation(value = "Returns user attributes and display name", notes = "A basic example of a resource returning user attributes from a social platform login.")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "User attributes returned") })
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON)
 	@OAuthSecurity(scope = "socialLogin")
-	public String hello() {
-		return "Hello " + securityContext.getAuthenticatedUser().getDisplayName();
+	public Map<String,Object> hello() {
+		AuthenticatedUser user = securityContext.getAuthenticatedUser();
+		Map<String, Object> userAttributes = user.getAttributes();
+		userAttributes.put("displayName", user.getDisplayName());
+		return userAttributes;
 	}
-
 }
