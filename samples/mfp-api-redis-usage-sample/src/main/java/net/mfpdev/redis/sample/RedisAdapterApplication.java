8f82110b91d9c4732b52b0af2757a7ce15f4e281
yotamm <yotamm@il.ibm.com>
2016-03-27 09:35:16 +0300

    change from com.ibm.mfp to net.mfpdev

--- samples/mfp-api-redis-usage-sample/src/main/java/net/mfpdev/redis/sample/RedisAdapterApplication.java
+++ samples/mfp-api-redis-usage-sample/src/main/java/net/mfpdev/redis/sample/RedisAdapterApplication.java
@@ -13,7 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package com.ibm.mfp.sample;
+package net.mfpdev.redis.sample;
 
 import com.ibm.mfp.adapter.api.ConfigurationAPI;
 import com.ibm.mfp.adapter.api.MFPJAXRSApplication;
--- samples/mfp-api-redis-usage-sample/src/main/java/net/mfpdev/redis/sample/RedisAdapterResource.java
+++ samples/mfp-api-redis-usage-sample/src/main/java/net/mfpdev/redis/sample/RedisAdapterResource.java
@@ -13,7 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package com.ibm.mfp.sample;
+package net.mfpdev.redis.sample;
 
 import com.ibm.mfp.adapter.api.AdaptersAPI;
 import com.ibm.mfp.adapter.api.ConfigurationAPI;
