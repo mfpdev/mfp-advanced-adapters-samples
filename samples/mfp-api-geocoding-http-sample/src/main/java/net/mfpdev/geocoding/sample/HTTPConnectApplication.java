20295bb9e88ff27c5ab051ea3512b65f2dd147bb
yotamm <yotamm@il.ibm.com>
2016-03-27 09:33:10 +0300

    change from com.ibm.mfp to net.mfpdev

--- samples/mfp-api-geocoding-http-sample/src/main/java/net/mfpdev/geocoding/sample/HTTPConnectApplication.java
+++ samples/mfp-api-geocoding-http-sample/src/main/java/net/mfpdev/geocoding/sample/HTTPConnectApplication.java
@@ -13,7 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package com.ibm.mobilefirst.sample;
+package net.mfpdev.geocoding.sample;
 
 import com.ibm.mfp.adapter.api.ConfigurationAPI;
 import com.ibm.mfp.adapter.api.MFPJAXRSApplication;
--- samples/mfp-api-geocoding-http-sample/src/main/java/net/mfpdev/geocoding/sample/HTTPConnectResource.java
+++ samples/mfp-api-geocoding-http-sample/src/main/java/net/mfpdev/geocoding/sample/HTTPConnectResource.java
@@ -13,7 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package com.ibm.mobilefirst.sample;
+package net.mfpdev.geocoding.sample;
 
 import com.ibm.mfp.adapter.api.AdaptersAPI;
 import com.ibm.mfp.adapter.api.ConfigurationAPI;
