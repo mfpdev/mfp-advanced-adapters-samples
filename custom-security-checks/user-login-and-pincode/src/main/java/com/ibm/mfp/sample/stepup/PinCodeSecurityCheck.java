0cda5ef1cb3540d0397e9b4224fca7679627760f
artems <artems@il.ibm.com>
2016-04-03 13:46:40 +0300

    enrolment sample

---
+++ custom-security-checks/user-login-and-pincode/src/main/java/com/ibm/mfp/sample/stepup/PinCodeSecurityCheck.java
@@ -0,0 +1,92 @@
+/*
+ * IBM Confidential OCO Source Materials
+ *
+ * 5725-I43 Copyright IBM Corp. 2006, 2016
+ *
+ * The source code for this program is not published or otherwise
+ * divested of its trade secrets, irrespective of what has
+ * been deposited with the U.S. Copyright Office.
+ */
+
+package com.ibm.mfp.sample.stepup;
+
+import com.ibm.mfp.security.checks.base.CredentialsValidationSecurityCheck;
+import com.ibm.mfp.server.security.external.checks.AuthorizationResponse;
+import com.ibm.mfp.server.security.external.checks.SecurityCheckReference;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * If the pin code is already registered - compare it to one submitted by the user<br/>
+ * If the pin code is not registered - make sure the user is logged in, and prompt him to set the pin code.
+ *
+ * @author artem on 4/3/16.
+ */
+public class PinCodeSecurityCheck extends CredentialsValidationSecurityCheck {
+
+    private static final String PIN_CODE_ATTRIBUTE = "pin_code";
+    private static final String ACTION_SET_PIN_CODE = "set_pin_code";
+    private static final String ACTION_GET_PIN_CODE = "get_pin_code";
+    private static final String ACTION_FIELD = "action";
+
+    @SecurityCheckReference
+    private transient UserLoginSecurityCheck userLogin;
+
+    private transient String registeredPinCode;
+
+    @Override
+    public void authorize(Set<String> scope, Map<String, Object> credentials, HttpServletRequest request, AuthorizationResponse response) {
+        registeredPinCode = registrationContext.getRegisteredProtectedAttributes().get(PIN_CODE_ATTRIBUTE);
+
+        if (registeredPinCode == null)
+            // if the pin code is not registered - try to register it
+            registerPinCode(scope, credentials, request, response);
+        else
+            // if the pin code is registered - continue as usual
+            super.authorize(scope, credentials, request, response);
+    }
+
+    protected boolean validateCredentials(Map<String, Object> credentials) {
+        boolean ok = registeredPinCode.equals(credentials.get(ACTION_GET_PIN_CODE));
+        if (ok)
+            authorizationContext.setActiveUser(userLogin.getRegisteredUser());
+        return ok;
+    }
+
+    protected Map<String, Object> createChallenge() {
+        return createChallenge(ACTION_GET_PIN_CODE);
+    }
+
+    private void registerPinCode(Set<String> scope, Map<String, Object> credentials, HttpServletRequest request, AuthorizationResponse response) {
+        // before asking to set pin code make sure the user is logged in
+        if (!userLoggedIn(scope, credentials, request, response))
+            return;
+
+        // set the submitted pin code or challenge for one
+        String pinCode = credentials == null ? null : (String) credentials.get(ACTION_SET_PIN_CODE);
+        if (pinCode == null) {
+            // no pin code submitted, ask for one
+            response.addChallenge(getName(), createChallenge(ACTION_SET_PIN_CODE));
+        } else {
+            // pin code submitted, register it, and report success
+            registrationContext.getRegisteredProtectedAttributes().put(PIN_CODE_ATTRIBUTE, pinCode);
+            setState(STATE_SUCCESS);
+            response.addSuccess(scope, getExpiresAt(), getName());
+        }
+    }
+
+    private boolean userLoggedIn(Set<String> scope, Map<String, Object> credentials, HttpServletRequest request, AuthorizationResponse response) {
+        if (userLogin.isExpired())
+            userLogin.authorize(scope, credentials, request, response);
+        return userLogin.isAuthenticated();
+    }
+
+    private Map<String, Object> createChallenge(String action) {
+        Map<String, Object> challenge = new HashMap<String, Object>();
+        challenge.put(ACTION_FIELD, action);
+        return challenge;
+    }
+}
